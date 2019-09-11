c8e4a78d2f65f06ec8758c5d0469b4efed841898
Mithil.Kumar <mithil.kumar@MithilKumars-MacBook-Pro.local>
2019-09-11 15:07:00 +0530

    [Mithil] Refactor. Rename and Rewrite the groupexpense test by using mock.

--- src/test/java/GroupExpenseTest.java
+++ src/test/java/GroupExpenseTest.java
@@ -1,25 +1,22 @@
 import model.Expense;
 import model.GroupExpense;
-import model.Money;
-import model.Person;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class GroupExpenseTest {
     @Test
-    void expectAllPersonsEffectiveMoneyWhoInvolvedInGroupExpense() {
-        Person personA = new Person("personA");
-        Person personB = new Person("personB");
-        Person personC = new Person("personC");
-        Person personD = new Person("personD");
-        Expense expense1 = new Expense(personA, new Money(90.0), Arrays.asList(personA, personB, personC));
-        Expense expense2 = new Expense(personB, new Money(120.0), Arrays.asList(personA, personB, personC, personD));
-        Expense expense3 = new Expense(personC, new Money(60.0), Arrays.asList(personA, personB, personC));
+    void expectSplitIsCalledForEachExpenseWhenSplitExpenseIsCalledInGroupExpense() {
+        Expense expense1 = mock(Expense.class);
+        Expense expense2 = mock(Expense.class);
+        Expense expense3 = mock(Expense.class);
         GroupExpense trip = new GroupExpense();
         trip.splitExpense(Arrays.asList(expense1, expense2, expense3));
-        assertEquals("personA 10.0 personB 40.0 personC -20.0 personD -30.0", personA.personName + " " + personA.getEffectiveMoney() + " " + personB.personName + " " + personB.getEffectiveMoney() + " " + personC.personName + " " + personC.getEffectiveMoney() + " " + personD.personName + " " + personD.getEffectiveMoney());
+        verify(expense1).split();
+        verify(expense2).split();
+        verify(expense3).split();
     }
 }
