419cd92ce9adb54ad2210eaf44efbef10c99a6da
Mithil.Kumar <mithil.kumar@MithilKumars-MacBook-Pro.local>
2019-09-11 15:18:44 +0530

    [Mithil] Refactor.Format the code.

--- src/main/java/ExpenseMain.java
+++ src/main/java/ExpenseMain.java
@@ -9,19 +9,17 @@ import java.util.List;
 
 public class ExpenseMain {
     public static void main(String[] args) {
-        {
-            Person person1 = new Person("person1");
-            Person person2 = new Person("person2");
-            Person person3 = new Person("person3");
-            Display display=new Display();
-            Money moneySpent = new Money(90.0);
-            List<Person> personsInvolved=new ArrayList<>();
-            personsInvolved.add(person1);
-            personsInvolved.add(person2);
-            personsInvolved.add(person3);
-            Expense expense = new Expense(person1, moneySpent, Arrays.asList(person1, person2, person3));
-            expense.split();
-            display.display(personsInvolved);
-        }
+        Person person1 = new Person("person1");
+        Person person2 = new Person("person2");
+        Person person3 = new Person("person3");
+        Display display = new Display();
+        Money moneySpent = new Money(90.0);
+        List<Person> personsInvolved = new ArrayList<>();
+        personsInvolved.add(person1);
+        personsInvolved.add(person2);
+        personsInvolved.add(person3);
+        Expense expense = new Expense(person1, moneySpent, Arrays.asList(person1, person2, person3));
+        expense.split();
+        display.display(personsInvolved);
     }
 }
